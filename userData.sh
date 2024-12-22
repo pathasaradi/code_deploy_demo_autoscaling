8de932b695b7997adb490c90b43f39befc83a794
Challa Parthasaradi <117152440+pathasaradi@users.noreply.github.com>
2024-12-22 20:18:44 +0530

    Update userData.sh

--- userData.sh
+++ userData.sh
@@ -10,7 +10,7 @@ pip install awscli
 yum update
 yum install ruby wget -y
 cd /home/ec2-user
-wget https://aws-codedeploy-ap-southeast-1.s3.ap-southeast-1.amazonaws.com/latest/install
+wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
 chmod +x ./install
 ./install auto
 sudo service codedeploy-agent start
